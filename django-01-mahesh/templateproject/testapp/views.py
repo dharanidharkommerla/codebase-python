397400594e6c0134bfcf1a21c7b50e0b1938020c
dharanidharkommerla <dharanidharkommerla2001@gmail.com>
2025-07-20 19:00:41 +0530

    template project and template tags are utilized.

--- django-01-mahesh/templateproject/testapp/views.py
+++ django-01-mahesh/templateproject/testapp/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def wish(request):
-    return render(request, 'testapp/wish.html')
+    date = datetime.datetime.now()
+    msg = "Hello, World! Very Good "
+    h = int(date.strftime('%H'))
+    if h<12:
+        msg += 'Morning'
+    elif h<16:
+        msg += 'Afternoon'
+    elif h<21:
+        msg += 'Evening'
+    else:
+        msg += 'Night'
+
+    # dictionary
+    student_details = {
+        "name" : "sunny",
+        "rollno" : 101,
+        "marks" : 99
+    }
+    my_dict = {
+        'fulldatetime': date,
+        'msg':msg,
+        'name':student_details['name'],
+        'rollno':student_details['rollno'],
+        'marks':student_details['marks'],
+    }
+    return render(request, 'testapp/wish.html', context=my_dict)
+    # return render(request, 'testapp/wish.html', my_dict)
+    # return render(request, 'testapp/wish.html', {'insert_time': date,})
